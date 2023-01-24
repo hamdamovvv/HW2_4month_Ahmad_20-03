b882a2f7a99b5db2456e2fb22b0e251c98a02ccc
hamdamovvv <akhmadkhamdamov000@gmail.com>
2023-01-24 13:15:35 +0600

    Ahmad

--- src/components/Product/Product.jsx
+++ src/components/Product/Product.jsx
@@ -3,8 +3,8 @@ import React from 'react';
 import styles from './product.module.css';
 
 
-const Product = ({name, price, image}) => (
-    <div className={styles.mainProduct}>
+const Product = ({name, price, image, handleLike, id, isLiked}) => (
+    <div onClick={() => {handleLike(id)}} className={styles.mainProduct}>
         <div className={styles.productContainer}>
             <div className={styles.productIcon}>
                 <img src={image}  alt={name} className={styles.productImage}/>
@@ -12,6 +12,7 @@ const Product = ({name, price, image}) => (
             <div className={styles.productInfo}>
                 <h3>{name}</h3>
                 <p>Price: ${price}</p>
+                {isLiked && <p>liked</p>}
             </div>
         </div>
     </div>
